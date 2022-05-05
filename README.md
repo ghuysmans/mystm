199d6edce3416b159f409624765aea84389b5b8f
Guillaume Huysmans <ghuysmans99@gmail.com>
2022-05-05 23:49:47 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+inspired by https://github.com/michelp/pgfsm
+
+According to [this post](https://stackoverflow.com/questions/1003034/sql-case-vs-join-efficiency), we should prefer `JOIN` over `CASE`.
---
+++ schema.sql
@@ -0,0 +1,29 @@
+CREATE TABLE transition(
+  a ENUM('a', 'b', 'c'),
+  b ENUM('a', 'b', 'c'),
+  PRIMARY KEY (a, b)
+);
+
+CREATE TABLE machine(
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  state ENUM('a', 'b', 'c') NOT NULL DEFAULT 'a'
+);
+
+
+DELIMITER //
+
+CREATE TRIGGER machine_transition_i
+BEFORE INSERT ON machine
+FOR EACH ROW
+IF NEW.state <> 'a' THEN
+  SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT='Illegal initial state';
+END IF //
+
+CREATE TRIGGER machine_transition_u
+BEFORE UPDATE ON machine
+FOR EACH ROW BEGIN
+IF NOT EXISTS(SELECT * FROM transition WHERE a=OLD.state AND b=NEW.state) THEN
+  SET @msg = CONCAT('Illegal state transition: ', OLD.state, ' -> ', NEW.state);
+  SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT=@msg;
+END IF;
+END //
